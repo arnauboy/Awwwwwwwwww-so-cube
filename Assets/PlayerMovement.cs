e9c057c454648e39c338bf555b663e1dab288bf1
arnauboy <arnauboy@gmail.com>
2022-10-09 20:43:21 +0200

    player input left & right

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float forwardForce = 2000f;
+
+    public float sidewayForce = 500f;
+
+    bool moveLeft = false;
+
+    bool moveRight = false;
+
     // This is a reference to the Rightbody component called rb
     public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
+    }
 
+    void Update() {
+        moveRight = false;
+        moveLeft = false;
+        if(Input.GetKey("d")) {
+            moveRight = true;
+        }
+        if (Input.GetKey("a")) {
+            moveLeft = true;
+        }
     }
 
     // FixedUpdate is called once per frame and it is named this
     // way because we are dealing with physics
     void FixedUpdate()
     {
-        rb.AddForce(0,0,2000 * Time.deltaTime);
+        rb.AddForce(0,0,forwardForce * Time.deltaTime);
+
+        if(moveRight) {
+            rb.AddForce(sidewayForce, 0, 0);
+        }
+        if (moveLeft) {
+            rb.AddForce(-sidewayForce, 0, 0);
+        }
     }
 }
