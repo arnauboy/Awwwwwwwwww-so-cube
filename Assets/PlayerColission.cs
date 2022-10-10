7d1a83f5d253d2e3f5fe2cd1448d0e5b93ce8209
ArnaudMegaDude <arnauboy@gmail.com>
2022-10-10 20:35:34 +0200

    added collision and connecting scripts

---
+++ Assets/PlayerColission.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColission : MonoBehaviour
+{
+    public PlayerMovement movement;
+    void OnCollisionEnter(Collision collisionInfo) {
+        if (collisionInfo.collider.tag == "Obstacle") {
+            movement.enabled = false;
+        }
+    }
+}
